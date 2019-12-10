ff20c3088029db71c57cf84e11549f20d2296a81
Satish Nirankari <nirankari.satish@gmail.com>
2019-12-09 18:39:52 -0600

    This is Day 2 quick check

---
+++ QuickCheckUp.py
@@ -0,0 +1,8 @@
+print("Hello User!")
+Name=input("What is your Name?")
+print("Hello" + Name)
+Age=int(input("What is your Age?"))
+if Age<18:
+    print("You are just a Baby")
+else:
+    print("You are well traveled soul")
